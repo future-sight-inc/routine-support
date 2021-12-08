a0e302ab0bfdadc3fc8739aad2ce2be365c16f4a
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2021-12-08 21:59:03 +0300

    feat: fix calendar cell height

--- apps/web/src/features/week/components/WeekCalendar/components/ActivityCard/styled.ts
+++ apps/web/src/features/week/components/WeekCalendar/components/ActivityCard/styled.ts
@@ -31,7 +31,7 @@ export const Wrapper = styled(Box)<{
       ${rowStart} * ${theme.size.cellHeight} + ${marginTop} *
         ${theme.size.cellHeight} + 1px * ${rowStart}
     );
-    height: calc(${height} * ${theme.size.cellHeight});
+    height: calc(${height} * ${theme.size.cellHeight} + ${height}px - 1px);
     width: calc(100% / ${count} - 4px);
     left: calc(100% / ${count} * ${index} + 2px);
   `}
