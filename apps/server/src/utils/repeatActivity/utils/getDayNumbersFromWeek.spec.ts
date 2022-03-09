ee62574d70fd8c20fca571bac3284abc00a0575c
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-03-09 19:52:52 +0100

    style: move array on numbers to constants

--- apps/server/src/utils/repeatActivity/utils/getDayNumbersFromWeek.spec.ts
+++ apps/server/src/utils/repeatActivity/utils/getDayNumbersFromWeek.spec.ts
@@ -6,25 +6,31 @@ import {
 import { createMockWeekFromDateStringArray } from "../testUtils";
 import { getDayNumbersFromWeek } from "./getDayNumbersFromWeek";
 
+const DAY_NUMBERS_OF_WEEK_OF_MONTH = [7, 8, 9, 10, 11, 12, 13];
+const DAY_NUMBERS_OF_WEEK_BETWEEN_MONTHS = [28, 1, 2, 3, 4, 5, 6];
+const DAY_NUMBERS_OF_WEEK_BETWEEN_YEARS = [27, 28, 29, 30, 31, 1, 2];
+
 describe("getMonthsOfWeek", () => {
   it("Week of one month", () => {
     const week = createMockWeekFromDateStringArray(WEEK_OF_MONTH);
 
-    expect(getDayNumbersFromWeek(week)).toStrictEqual([
-      7, 8, 9, 10, 11, 12, 13,
-    ]);
+    expect(getDayNumbersFromWeek(week)).toStrictEqual(
+      DAY_NUMBERS_OF_WEEK_OF_MONTH
+    );
   });
 
   it("Week between two months", () => {
     const week = createMockWeekFromDateStringArray(WEEK_BETWEEN_MONTHS);
 
-    expect(getDayNumbersFromWeek(week)).toStrictEqual([28, 1, 2, 3, 4, 5, 6]);
+    expect(getDayNumbersFromWeek(week)).toStrictEqual(
+      DAY_NUMBERS_OF_WEEK_BETWEEN_MONTHS
+    );
   });
 
   it("Week between two months", () => {
     const week = createMockWeekFromDateStringArray(WEEK_BETWEEN_YEARS);
-    expect(getDayNumbersFromWeek(week)).toStrictEqual([
-      27, 28, 29, 30, 31, 1, 2,
-    ]);
+    expect(getDayNumbersFromWeek(week)).toStrictEqual(
+      DAY_NUMBERS_OF_WEEK_BETWEEN_YEARS
+    );
   });
 });
