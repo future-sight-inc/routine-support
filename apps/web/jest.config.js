e7904527a785a1c70438424c362333f276163be4
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-03-11 19:03:33 +0100

    chore: update jest configs

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  displayName: 'web',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|scss|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+  displayName: "web",
+  preset: "../../jest.preset.js",
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+    },
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/web',
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.[tj]s$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/apps/web",
 };
