365699344b5ccd9253c79529595be3ebc211fefb
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-03-19 00:25:02 +0100

    feat: use hash password

--- apps/server/src/routes/UserRouter.ts
+++ apps/server/src/routes/UserRouter.ts
@@ -2,31 +2,42 @@ import { UserModel } from "@routine-support/domains";
 import { Router } from "express";
 import { authorization } from "../middleware/authorization";
 import { getAuthCookie } from "../utils/getAuthCookie";
+import { hashPassword } from "../utils/hashPassword";
 
 export const userRouter = Router();
 
 userRouter.post("/", async (req, res) => {
-  UserModel.create(req.body, (err, result) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
+  const { password, ...data } = req.body;
 
-    const cookie = getAuthCookie(result);
-    return res.status(200).cookie(cookie.name, cookie.token).send(result);
-  });
+  UserModel.create(
+    { ...data, password: hashPassword(password) },
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      const cookie = getAuthCookie(result);
+      return res.status(200).cookie(cookie.name, cookie.token).send(result);
+    }
+  );
 });
 
 userRouter.post("/login", async (req, res) => {
-  UserModel.findOne(req.body, (err, result) => {
-    if (err || !result) {
-      return res.status(401).send(err);
+  const { email, password } = req.body;
+
+  UserModel.findOne(
+    { email, password: hashPassword(password) },
+    (err, result) => {
+      if (err || !result) {
+        return res.status(401).send(err);
+      }
+
+      const cookie = getAuthCookie(result);
+
+      return res.status(200).cookie(cookie.name, cookie.token).send(result);
     }
-
-    const cookie = getAuthCookie(result);
-
-    return res.status(200).cookie(cookie.name, cookie.token).send(result);
-  });
+  );
 });
 
 userRouter.get("/", authorization, (__, res) => {
