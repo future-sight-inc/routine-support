41b202f7c67b859c975a857a7799ec806dedf25c
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-03-19 01:39:14 +0100

    feat: use default date picker

--- apps/web/src/components/FormFields/DatePicker/DatePicker.tsx
+++ apps/web/src/components/FormFields/DatePicker/DatePicker.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import UncontrolledDatePicker from "@mui/lab/DatePicker";
 import { TextField } from "@mui/material";
 import { Moment } from "moment";
+import moment from "moment";
 
 import { Controller } from "../Controller";
 import { FormFieldProps } from "../types";
@@ -26,13 +26,19 @@ export const DatePicker: React.FC<DatePickerProps> = ({
       label={label}
       required={required}
       disabled={disabled}
-      render={({ field, fieldState }) => (
-        <UncontrolledDatePicker
-          renderInput={(props) => (
-            <TextField {...props} error={Boolean(fieldState.error)} />
-          )}
-          minDate={minDate}
+      render={({ field: { value, onChange, ...field }, fieldState }) => (
+        <TextField
+          type="date"
+          InputLabelProps={{
+            shrink: true,
+          }}
           {...field}
+          // todo make more clear
+          value={value.format("YYYY-MM-DD")}
+          onChange={(event) =>
+            onChange(moment(event.target.value, "YYYY-MM-DD"))
+          }
+          error={Boolean(fieldState.error)}
         />
       )}
     />
