75c244294936420c207982543886b75949c99be8
glockstory <danila1025@gmail.com>
2022-03-20 17:45:37 +0100

    test: add situations with minutes

--- apps/server/src/utils/validateActivity.spec.ts
+++ apps/server/src/utils/validateActivity.spec.ts
@@ -43,4 +43,32 @@ describe("validateActivity", () => {
 
     expect(validationData.isValid).toBeTruthy();
   });
+
+  it("Start time is less then end time in minutes", () => {
+    const activity = createMockActivitySchema();
+    setActivityTime(
+      {
+        start: "10:10",
+        end: "10:11",
+      },
+      activity
+    );
+    const validationData = validateActivity(activity);
+
+    expect(validationData.isValid).toBeTruthy();
+  });
+
+  it("Start time is greater then end time in minutes", () => {
+    const activity = createMockActivitySchema();
+    setActivityTime(
+      {
+        start: "11:11",
+        end: "11:10",
+      },
+      activity
+    );
+    const validationData = validateActivity(activity);
+
+    expect(validationData.isValid).toBeFalsy();
+  });
 });
