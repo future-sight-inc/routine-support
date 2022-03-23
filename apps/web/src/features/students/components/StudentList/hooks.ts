ffeae70a75f278ced06a67837ab971be29794ae9
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-03-23 22:11:52 +0100

    feat: add current student name into qr modal

--- apps/web/src/features/students/components/StudentList/hooks.ts
+++ apps/web/src/features/students/components/StudentList/hooks.ts
@@ -1,16 +1,18 @@
 import { useState } from "react";
 
+import { Student } from "@routine-support/domains";
 import { Id } from "@routine-support/types";
 import QRCode from "qrcode";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 import { StudentListActions } from "./StudentList";
 
 export const useStudentListComponent = (actions: StudentListActions) => {
   const [qr, setQR] = useState<string | null>(null);
+  const [currentStudent, setCurrentStudent] = useState<Student | null>(null);
   const [qrLoading, setQrLoading] = useState(false);
-  const { t } = useTranslation()
-  
+  const { t } = useTranslation();
+
   const onStudentDelete = async (id: Id) => {
     if (window.confirm(t("Confirm student delete"))) {
       await actions.deleteStudent(id);
@@ -19,13 +21,14 @@ export const useStudentListComponent = (actions: StudentListActions) => {
     }
   };
 
-  const onQrOpen = async (id: Id) => {
+  const onQrOpen = async (student: Student) => {
     try {
       setQrLoading(true);
 
-      const qr = await QRCode.toDataURL(JSON.stringify({ id }));
+      const qr = await QRCode.toDataURL(JSON.stringify({ id: student._id }));
 
       setQR(qr);
+      setCurrentStudent(student);
     } catch (error) {
       console.error(error);
     } finally {
@@ -35,12 +38,13 @@ export const useStudentListComponent = (actions: StudentListActions) => {
 
   const onQrClose = () => {
     setQR(null);
+    setCurrentStudent(null);
   };
 
   const onStudentClick = actions.openStudentModal;
 
   return {
-    models: { qr, qrLoading },
+    models: { qr, qrLoading, currentStudent },
     operations: { onStudentClick, onStudentDelete, onQrOpen, onQrClose },
   };
 };
