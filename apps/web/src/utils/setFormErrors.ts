e8c8b73bc4ec513bb385cff4fe881118224357b9
Alexander <alex_tokarev01@mail.ru>
2022-04-05 15:00:10 +0300

    feat: delete comments for set submit error message

--- apps/web/src/utils/setFormErrors.ts
+++ apps/web/src/utils/setFormErrors.ts
@@ -3,16 +3,12 @@ import { FormError, SubmitErrorData } from "@routine-support/types";
 export const setFormErrors = (
   submitErrorData: SubmitErrorData = { isValid: true },
   setError: (name: string, error: FormError) => void,
-  // setSubmitError
   setSubmitError: (error: string) => void
 ) => {
-  // если есть ошибка формы ()
-  // положи ошибку в submitError
   submitErrorData.error
     ? setSubmitError(submitErrorData.error)
     : setSubmitError("");
   submitErrorData.errors?.forEach((error) => {
-    // setSubmitError(error.message);
     setError(error.name, error);
   });
 };
