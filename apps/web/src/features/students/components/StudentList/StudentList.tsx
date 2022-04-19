685ffc02bb7419fa3028b8c05ded18433e4371bc
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-04-19 14:23:15 +0200

    feat: update qr modal styles

--- apps/web/src/features/students/components/StudentList/StudentList.tsx
+++ apps/web/src/features/students/components/StudentList/StudentList.tsx
@@ -47,15 +47,17 @@ export const StudentList: React.FC<StudentListProps> = ({
         />
       ))}
       <Modal isOpened={Boolean(qr)} onClose={onQrClose}>
-        <S.QrTitle>
-          {t("QR code for")} {currentStudent?.name}
-        </S.QrTitle>
-        {qr && <S.Qr src={qr} />}
-        <S.QrTitle>
-          {t("QR instructions start")}
-          <br />
-          {t("QR instructions end")}
-        </S.QrTitle>
+        <S.ModalContent>
+          <S.ModalText>
+            {t("QR code for")} {currentStudent?.name}
+          </S.ModalText>
+          {qr && <S.Qr src={qr} />}
+          <S.ModalText>
+            {t("QR instructions start")}
+            <br />
+            {t("QR instructions end")}
+          </S.ModalText>
+        </S.ModalContent>
       </Modal>
     </S.Wrapper>
   );
