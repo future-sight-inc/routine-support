96def07c26f1ed5d7f8bbdd6652fdc21d9912cc2
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-05-10 18:08:35 +0200

    feat: split workspace json

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-  ],
-  typescript: { reactDocgen: false },
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
