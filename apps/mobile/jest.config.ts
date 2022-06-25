4b1a5c892ea04f2ff4c5698b9100dc63eef7c7c1
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-06-25 13:39:35 +0300

    feat: migrate jest config

--- apps/mobile/jest.config.ts
+++ apps/mobile/jest.config.ts
@@ -1,16 +1,18 @@
 module.exports = {
   displayName: "mobile",
-  preset: "react-native",
-  testRunner: "jest-jasmine2",
   resolver: "@nrwl/jest/plugins/resolver",
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
   moduleFileExtensions: ["ts", "js", "html", "tsx", "jsx"],
   setupFilesAfterEnv: ["<rootDir>/test-setup.ts"],
   moduleNameMapper: {
-    ".svg": "@nrwl/react-native/plugins/jest/svg-mock",
+    ".svg": "@nrwl/expo/plugins/jest/svg-mock",
   },
   transform: {
     "\\.(js|ts|tsx)$": require.resolve("react-native/jest/preprocessor.js"),
-    "^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$": require.resolve(
+    "^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp|ttf)$": require.resolve(
       "react-native/jest/assetFileTransformer.js"
     ),
   },
