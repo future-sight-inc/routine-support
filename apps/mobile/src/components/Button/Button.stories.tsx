22be28a41ff27eed1141e3092f70d8e055e5509e
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-06-27 13:49:08 +0300

    feat: migrate button and typography from native-storybook branch

--- apps/mobile/src/components/Button/Button.stories.tsx
+++ apps/mobile/src/components/Button/Button.stories.tsx
@@ -1,42 +1,42 @@
-// import { storiesOf } from "@storybook/react-native";
-// import React from "react";
-// import { Dimensions, StyleSheet, View } from "react-native";
-// import { Button } from "./Button";
+import { storiesOf } from "@storybook/react-native";
+import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { Button } from "./Button";
 
-// const styles = StyleSheet.create({
-//   layout: {
-//     paddingHorizontal: 16,
-//     flexDirection: "row",
-//     flexWrap: "wrap",
-//     width: Dimensions.get("window").width - 32,
-//   },
-//   button: {
-//     marginLeft: 16,
-//     marginBottom: 8,
-//   },
-// });
+const styles = StyleSheet.create({
+  layout: {
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: Dimensions.get("window").width - 32,
+  },
+  button: {
+    marginLeft: 16,
+    marginBottom: 8,
+  },
+});
 
-// storiesOf("Components", module).add("Button", () => (
-//   <>
-//     <View style={styles.layout}>
-//       <Button style={styles.button} text="Primary Button" />
-//       <Button
-//         variant="secondary"
-//         style={styles.button}
-//         text="Secondary Button"
-//       />
-//     </View>
-//     <View style={styles.layout}>
-//       <Button style={styles.button} text="Disabled Primary" disabled />
-//       <Button
-//         variant="secondary"
-//         style={styles.button}
-//         text="Disabled Secondary"
-//         disabled
-//       />
-//     </View>
-//     <View style={styles.layout}>
-//       <Button style={styles.button} text="Full Width" fullWidth />
-//     </View>
-//   </>
-// ));
+storiesOf("Components", module).add("Button", () => (
+  <>
+    <View style={styles.layout}>
+      <Button style={styles.button} text="Primary Button" />
+      <Button
+        variant="secondary"
+        style={styles.button}
+        text="Secondary Button"
+      />
+    </View>
+    <View style={styles.layout}>
+      <Button style={styles.button} text="Disabled Primary" disabled />
+      <Button
+        variant="secondary"
+        style={styles.button}
+        text="Disabled Secondary"
+        disabled
+      />
+    </View>
+    <View style={styles.layout}>
+      <Button style={styles.button} text="Full Width" fullWidth />
+    </View>
+  </>
+));
--- apps/mobile/src/components/Typography/Typography.stories.tsx
+++ apps/mobile/src/components/Typography/Typography.stories.tsx
@@ -17,6 +17,38 @@ const styles = StyleSheet.create({
 
 storiesOf("Components", module).add("Typography", () => (
   <View style={styles.layout}>
-    <Typography />
+    <Typography variant="caption4" style={styles.text}>
+      Caption4/20px/bold
+    </Typography>
+    <Typography variant="text1" style={styles.text}>
+      Text1/16px/normal
+    </Typography>
+    <Typography variant="text2" style={styles.text}>
+      Text2/14px/normal
+    </Typography>
+    <Typography variant="text3" style={styles.text}>
+      Text3/12px/normal
+    </Typography>
+    <Typography variant="text1Bold" style={styles.text}>
+      Text1Bold/16px/bold
+    </Typography>
+    <Typography variant="text2Bold" style={styles.text}>
+      Text2Bold/14px/bold
+    </Typography>
+    <Typography variant="text3Bold" style={styles.text}>
+      Text3Bold/12px/bold
+    </Typography>
+    <Typography variant="text2" style={styles.text}>
+      Normal
+    </Typography>
+    <Typography variant="text2" color="primary" style={styles.text}>
+      Primary
+    </Typography>
+    <Typography variant="text2" color="secondary" style={styles.text}>
+      Secondary
+    </Typography>
+    <Typography variant="text2" color="error" style={styles.text}>
+      Error
+    </Typography>
   </View>
 ));
