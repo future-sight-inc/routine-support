880f5df427df8c28ff9fb65177d36619693b4a60
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-07-18 23:56:55 +0300

    test: add tests to date selector component

---
+++ apps/mobile/src/components/DateSelector/locators.ts
@@ -0,0 +1,8 @@
+export enum DateSelectorLocators {
+  PressElement = "DateSelectorPressElement",
+  Background = "DateSelectorBackground",
+  Modal = "DateSelectorModal",
+  ModalDim = "DateSelectorModalDim",
+  CloseText = "DateSelectorCloseText",
+  ConfirmText = "DateSelectorConfirmText",
+}
