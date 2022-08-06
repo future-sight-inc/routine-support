c2aa7b5db30cbaf1de45753ffb4d64878585c0e9
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2022-08-06 18:34:42 +0300

    refactor: turn activity filter into array

--- libs/domains/src/lib/week/api.ts
+++ libs/domains/src/lib/week/api.ts
@@ -4,7 +4,11 @@ import { ActivityFilter, WeekNumber, WeekSchema, YearNumber } from "./types";
 import { createActivityFilterParam } from "./utils/createActivityFilterParam";
 
 export const createCoachWeekAPI = (client: AxiosInstance) => ({
-  getWeek: async (year: YearNumber, week: WeekNumber, filter?: ActivityFilter): Promise<WeekSchema> => {
+  getWeek: async (
+    year: YearNumber,
+    week: WeekNumber,
+    filter: ActivityFilter
+  ): Promise<WeekSchema> => {
     const response: AxiosResponse<WeekSchema> = await client.get(`/week/${year}/${week}`, {
       params: { filter: createActivityFilterParam(filter) },
     });
