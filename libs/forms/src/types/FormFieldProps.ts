2fd13e365ba8832da6c6e8923d88952fe46f15d6
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2023-01-26 19:41:54 +0300

    feat: remove any typing from form field props

--- libs/forms/src/types/FormFieldProps.ts
+++ libs/forms/src/types/FormFieldProps.ts
@@ -2,7 +2,7 @@ import { Control } from "react-hook-form";
 
 export interface FormFieldProps {
   name: string;
-  control: Control<any>; // todo
+  control: Control;
   label?: string;
   required?: boolean;
   disabled?: boolean;
