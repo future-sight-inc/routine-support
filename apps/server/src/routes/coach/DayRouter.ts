d6b84f80631d1ee39b0b9dcab71fc9bfc86f996e
Nikita Dmitriev <nikitadmitriev97@mail.ru>
2023-04-03 20:39:52 +0300

    fix: resolve date migration problem in day router

--- apps/server/src/routes/coach/DayRouter.ts
+++ apps/server/src/routes/coach/DayRouter.ts
@@ -1,8 +1,8 @@
 import { DayJson, stringifyDay } from "@routine-support/domains";
-import { parseDate, stringifyDate } from "@routine-support/utils";
+import { parseDate } from "@routine-support/utils";
 import { Response, Router } from "express";
+import { ActivityModel } from "../../db/models/Activity";
 import { filterActivities } from "../../utils/filterActivities";
-import { getActivitiesOfWeek } from "../../utils/getActivitiesOfWeek";
 import { getTimeRange } from "../../utils/getTimeRange";
 import { parseActivitiesFilter } from "../../utils/parseActivitiesFilter";
 
@@ -11,18 +11,12 @@ export const dayRouter = Router();
 dayRouter.get("/:date", async (req, res: Response<DayJson>) => {
   const { date } = req.params;
   const currentDate = parseDate(date);
-  const coach = res.locals.coach;
 
   const { filter } = req.query;
   const parsedFilter = parseActivitiesFilter(filter as string);
 
-  const activitiesOfWeek = await getActivitiesOfWeek({
-    currentDate,
-    coachId: coach._id,
-  });
-  const todaysActivities = activitiesOfWeek.filter(
-    (activity) => stringifyDate(activity.date) === date
-  );
+  const todaysActivities = await ActivityModel.find({ date: currentDate }).lean({ getters: true });
+
   const filteredActivities = filterActivities(todaysActivities, parsedFilter);
 
   return res.status(200).send(
